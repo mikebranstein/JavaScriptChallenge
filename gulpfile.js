2c250f2d38336a9d29496256cf3fb65a0f17553b
Tyler <ty8356@gmail.com>
2017-04-26 14:41:07 -0400

    Added src and dist directories. Added package.json, .gitignore, and gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var del = require('del');
+var deploy = require('gulp-gh-pages');
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src",
+            index: "challenge.html"
+        }
+    });
+});
+
+gulp.task('clean:dist', function() {
+    return del.sync(['dist/**', '!dist']);
+});
+
+gulp.task('copy', function() {
+    return gulp.src('./src/**/*')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(deploy())
+});
+
+gulp.task('publish', function() {
+    runSequence(
+        'clean:dist',
+        'copy',
+        'deploy')
+});
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('src/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
