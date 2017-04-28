46ff621c33de5e4ade70287667fc1cf8491c5e57
Tyler <ty8356@gmail.com>
2017-04-28 16:43:36 -0400

    Update 2017-04-28T20:43:34.471Z

---
+++ js/challenge.js
@@ -0,0 +1,42 @@
+var countDownDate = addMinutes(new Date(), .316);
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('timer').innerHTML = minutes + ':' + seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    
+    if (distance < 1000) {
+        clearInterval(x);
+        window.location = 'fail.html';
+    }
+}, 1000);
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+window.onload = function() {
+    document.getElementById("testButton").addEventListener("click", function(){
+        var code = document.getElementById("editor").value;
+        
+        var array = [2,3,7,1,5,6,0].toString();
+        var result = eval(code + '\n challenge([' + array + ']);');
+        var correctResult = [0,1,2,3,5,6,7];
+
+        if (!Array.isArray(result)) {
+            alert('Does not return an array!'); 
+            return;
+        }
+        
+        if (result.length == correctResult.length 
+        && result.every(function(element, index) { return element === correctResult[index]; })) {
+            window.location = 'success.html';
+        } else {
+            alert('INCORRECT');
+        }
+    });
+}
