89f4185787fc4107de77fbeb628359f01852e283
Chase Guggenheim <guggencb@miamioh.edu>
2017-06-01 13:38:02 -0400

    Changes to the ace editor text in the challenges html ; changes to the size of the yellow title text ; instructions made simpler, added two min warning to all instucitons; added a 22 char limit to names on instuction.js ; scoreboard doesnt print expired times or messed u times

--- src/js/scoreboard.js
+++ src/js/scoreboard.js
@@ -7,13 +7,15 @@ window.onload = function () {
     function loadScores(scores, board) {
         if (scores !== null) {
             var arr = new Array();
+            //converts the time from string to int value, for desc order
             scores.forEach(function (item) {
                 seconds = (item.time.substring(0, 1) * 60) + item.time.substring(2);
-                arr.push({ name: item.name, time: item.time, seconds: seconds });
-            }, this); 
+                if (seconds > 0)
+                    arr.push({ name: item.name, time: item.time, seconds: seconds });
+            }, this);
             arr.sort(compareInt);
             arr.forEach(function (item) {
-                board.innerHTML += "<div class=\"row\"> <div class=\" col-md-8 col-sm-6 \" style=\"text-align:right\">" + item.name + "</div> <div class=\"col-md-4 col-sm-4\">" + item.time + "</div></div>";
+                board.innerHTML += "<div class=\"row\"> <div class=\" col-md-8 col-sm-6 \" style=\"text-align:left;\">" + item.name + "</div> <div class=\"col-md-4 col-sm-4\">" + item.time + "</div></div>";
             }, this);
         }
         function compareInt(B, A) {
