9d9b15a4d8e01d45787804d051c962125f965e9d
Chase Guggenheim <guggencb@miamioh.edu>
2017-06-01 15:50:42 -0400

    added css styling to the scoreboard titles ; moved id tags in index ; scorboard.html and index.html now automatically refresh the scores every 5 seconds, changes were made in scoreboard.js

--- src/js/scoreboard.js
+++ src/js/scoreboard.js
@@ -1,25 +1,32 @@
 window.onload = function () {
+    function refresh() {
+        loadScores(JSON.parse(localStorage.getItem('challengeOneScores')), document.getElementById('challengeOneTimes'));
+        loadScores(JSON.parse(localStorage.getItem('challengeTwoScores')), document.getElementById('challengeTwoTimes'));
+        loadScores(JSON.parse(localStorage.getItem('challengeThreeScores')), document.getElementById('challengeThreeTimes'));
 
-    loadScores(JSON.parse(localStorage.getItem('challengeOneScores')), document.getElementById('challengeOneTimes'));
-    loadScores(JSON.parse(localStorage.getItem('challengeTwoScores')), document.getElementById('challengeTwoTimes'));
-    loadScores(JSON.parse(localStorage.getItem('challengeThreeScores')), document.getElementById('challengeThreeTimes'));
-
-    function loadScores(scores, board) {
-        if (scores !== null) {
-            var arr = new Array();
-            //converts the time from string to int value, for desc order
-            scores.forEach(function (item) {
-                seconds = (item.time.substring(0, 1) * 60) + item.time.substring(2);
-                if (seconds > 0)
-                    arr.push({ name: item.name, time: item.time, seconds: seconds });
-            }, this);
-            arr.sort(compareInt);
-            arr.forEach(function (item) {
-                board.innerHTML += "<div class=\"row\"> <div class=\" col-md-8 col-sm-6 \" style=\"text-align:left;\">" + item.name + "</div> <div class=\"col-md-4 col-sm-4\">" + item.time + "</div></div>";
-            }, this);
-        }
-        function compareInt(B, A) {
-            return parseInt(A.seconds) - parseInt(B.seconds);
+        function loadScores(scores, board) {
+            board.innerHTML = "";
+            if (scores !== null) {
+                var arr = new Array();
+                //converts the time from string to int value, for desc order
+                scores.forEach(function (item) {
+                    seconds = (item.time.substring(0, 1) * 60) + item.time.substring(2);
+                    if (seconds > 0)
+                        arr.push({ name: item.name, time: item.time, seconds: seconds });
+                }, this);
+                arr.sort(compareInt);
+                arr.forEach(function (item) {
+                    board.innerHTML += "<div class=\"row\"> <div class=\" col-md-8 col-sm-6 \" style=\"text-align:left;\">" + item.name + "</div> <div class=\"col-md-4 col-sm-4\">" + item.time + "</div></div>";
+                }, this);
+            }
+            function compareInt(B, A) {
+                return parseInt(A.seconds) - parseInt(B.seconds);
+            }
         }
     }
+    refresh();
+setInterval(function(){
+    refresh();
+}, 5000)
 }
+
