85be7047842430a47c9c4e94259b406fad5a46c6
Mike Branstein <mike.branstein@gmail.com>
2017-06-06 15:28:16 -0400

    Update 2017-06-06T19:28:11.978Z

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function saveSelection(e) {
-    var challengeNumber = e.id;
-
-    localStorage.setItem('challengeNumber', challengeNumber);
-    window.location = 'instructions'+challengeNumber+'.html';
-}
--- js/results.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    document.getElementById('returnButton').addEventListener('click', returnToIndex);
-}
-
-function returnToIndex() {
-    window.location = 'index.html';
-}
